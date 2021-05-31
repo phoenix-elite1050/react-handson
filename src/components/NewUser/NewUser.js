327d6752cc08ff26392f9925dae88c70a8059ef9
parag-acellere <parag.dalal@embold.io>
2021-05-31 17:04:34 +0530

    Update NewUser.js

--- src/components/NewUser/NewUser.js
+++ src/components/NewUser/NewUser.js
@@ -1,7 +1,7 @@
 import UserForm from "./UserForm";
 const NewUser = (props)=>{
     const formHandler =(data)=>{
-        console.log(data);
+        //console.log(data);
     }
     return (<div className="new-user">
         <UserForm formHandler={props.newUserData}/>
