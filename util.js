dc94360d6b107981d606baf98f90b0f6b3a3bb18
parag-acellere <parag.dalal@embold.io>
2021-07-05 18:13:06 +0530

    Update util.js

--- util.js
+++ util.js
@@ -1,5 +1,6 @@
 function DateFormater(props){
     let b;
+    let a;
     console.log("Ad");
     const month = props.date.toLocaleString("en-us",{month:'long'})
     const date = props.date.toLocaleString("en-us",{day:'2-digit'})
