485b499c2dbd5f8e161bbdf06f37c469b1a54898
INPUN5CD024HH4L\Parag <parag.dalal@embold.io>
2022-02-09 18:20:42 +0530

    TEST-3

--- src/AvoidFilecreateTempFile.java
+++ src/AvoidFilecreateTempFile.java
@@ -39,5 +39,7 @@ public class AvoidFilecreateTempFile extends BaseChecker {
 		codeIssuePublisher.publish(new CodeIssueModel(context.getCu().getCuFile(), node.getRange().begin.line,
 				Severity.high, "Avoid FilecreateTempFile",
 				CodeIssueNames.AVOID_FILE_CREATE_TEMP_FILE));
+				
+		System.out.println("");
 	}
 }
