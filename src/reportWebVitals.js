ab99e0ba064d37a6980035b66d1a7239b7e7caa0
INPUN5CD024HH4L\Parag <parag.dalal@embold.io>
2022-02-09 13:28:05 +0530

    ddd

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -12,12 +12,14 @@ const reportWebVitals = onPerfEntry => {
 
 function abc2(){
   console.log("2")
-  console.log("2")
-  console.log("2")
 }
 export default reportWebVitals;
 function asdasd(){
   console.log("233")
   console.log("233")
+  console.log("34234")
+  console.log("34234")
+  console.log("34234")
+  console.log("34234")
   console.log("34234")
 }
